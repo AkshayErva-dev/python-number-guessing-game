b229de1d06a6b18cf33896433517450dc965093e
AkshayErva-dev <20240802768@dypiu.ac.in>
2026-03-01 10:51:57 +0530

    Added number guessing game project

---
+++ numbergame.py
@@ -0,0 +1,15 @@
+import random
+
+num = random.randint(1,30)
+guess = "none"
+while guess != num:
+    guess = int(input("Enter the number : "))
+    if guess == num:
+        print("Congratulations!")
+        print("You have guessed the correct number")
+    elif guess > num:
+        print("Oops, you guessed the wrong number")
+        print("Please Guess a lower number")
+    else:
+         print("Oops, you guessed the wrong number")
+         print("Please Guess a higher number")
